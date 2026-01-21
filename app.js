389188c82c8b37a4b265255671b878cd3a136556
prathmeshwake <prathmeshwake123@gmail.com>
2026-01-21 21:15:27 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const PORT = 8080;
+
+app.get('/', (req, res) => {
+  res.send('🚀 Deployed to OpenShift using GitHub Actions + Docker Hub!');
+});
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}`);
+});
