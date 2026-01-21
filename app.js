87440ac34c517f111dc45c6361c8d91fc4b289c4
prathmeshwake <prathmeshwake123@gmail.com>
2026-01-21 21:43:26 +0530

    changed app.js file and redeployed it again

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = 8080;
 
 app.get('/', (req, res) => {
-  res.send('Hello Deployed to OpenShift using GitHub Actions + Docker Hub!');
+  res.send('Hi Bro I have made the changes and redeployed this app again now!');
 });
 
 app.listen(PORT, () => {
